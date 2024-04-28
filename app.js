fa35518a691704a1042c9e1d04fadb798c726309
vanger2607 <vanger2607@gmail.com>
2024-04-28 19:45:10 +0300

    init project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+// Импортируем middleware
+const bodyParser = require("body-parser");
+const { cors } = require("./middlewares");
+
+const PORT = 3000;
+const app = express();
+
+app.use(
+  cors,
+  bodyParser.json(),
+  express.static(path.join(__dirname, "public")),
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+});

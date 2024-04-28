3666e065df6d8eae888fc77f16b794bcba22aa23
vanger2607 <vanger2607@gmail.com>
2024-04-28 20:45:31 +0300

    feat: set up app.js

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
 
-const CONFIG = require("./config")
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
+
+const CONFIG = require('./config')
+
 const app = express();
-console.log(CONFIG)
+
+connectToDatabase();
+
 app.use(
-  express.static(path.join(__dirname, "public")),
+  cors, 
+  bodyParser.json(),
+  express.static(path.join(__dirname, 'public')),
+  usersRouter, 
+  gamesRouter, 
+  categoriesRouter
 );
 
-app.listen(CONFIG.PORT, () => {
-  console.log(`Server is running at PORT http://localhost:${CONFIG.PORT}`);
-});
+app.listen(CONFIG.PORT); 

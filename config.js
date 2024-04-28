7c24e6171ef3910585bdfcbd5c9fc62eec5fed64
vanger2607 <vanger2607@gmail.com>
2024-04-28 20:13:49 +0300

    feat: add config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+module.exports = {
+  port: process.env.PORT
+};
+
+// не решил, что лучше, есть еще такой вариант:
+/*
+const dotenv = require('dotenv');
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
+const { parsed: envs } = result;
+console.log(envs);
+module.exports = envs;*/
+/* если переменных станет слишком много и словарик exports из понятного и читаемого превратиться в ужас,
+  воспользуюсь вторым вариантом*/

ffe1a77d52886e8648c98c93bb56297d4db7714d
vanger2607 <vanger2607@gmail.com>
2024-05-02 19:21:32 +0300

    feat: add get-queries to routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
-const usersRouter = require("express").Router();
-
-usersRouter.get("/users", () => {});
-
-module.exports = usersRouter;
+ // Создаём роут для запросов категорий 
+ const usersRouter = require('express').Router();
+  
+ // Импортируем вспомогательные функции
+ const findAllUsers = require('../middlewares/');
+ const sendAllUsers = require('../controllers/');
+ 
+ // Обрабатываем GET-запрос с роутом '/categories'
+ usersRouter.get('/users', findAllUsers, sendAllUsers);
+ 
+ // Экспортируем роут для использования в приложении — app.js
+ module.exports = usersRouter;

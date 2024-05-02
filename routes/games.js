4f3e791c9db1f14e2904cf2f585e5d76edb934d1
vanger2607 <vanger2607@gmail.com>
2024-05-02 20:09:41 +0300

    fix: fix import

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,6 @@
 const gamesRouter = require('express').Router();
 
-const findAllGames = require('../middlewares').findAllGames;
+const findAllGames = require('../midllewares').findAllGames;
 const sendAllGames = require('../controllers').sendAllGames;
 
 gamesRouter.get('/games', findAllGames, sendAllGames);

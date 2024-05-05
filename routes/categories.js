04e672dbd4a8d094ab3cad8fc405d287d9f927fd
vanger2607 <vanger2607@gmail.com>
2024-05-05 23:51:36 +0300

    feat: add put-route categories/id

--- routes/categories.js
+++ routes/categories.js
@@ -2,9 +2,9 @@
 const categoriesRouter = require('express').Router();
 // Импортируем вспомогательные функции
 
-const { findAllCategories, createCategory, findCategoryById } = require('../midllewares');
+const { findAllCategories, createCategory, findCategoryById, updateCategory } = require('../midllewares');
 
-const { sendAllCategories, sendCategoryCreate, sendCategoryById } = require('../controllers');
+const { sendAllCategories, sendCategoryCreate, sendCategoryById, sendCategoryUpdated } = require('../controllers');
 
 // Обрабатываем GET-запрос с роутом '/categories'
 categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
@@ -15,7 +15,11 @@ categoriesRouter.post(
     createCategory,
     sendCategoryCreate
 );
-
+categoriesRouter.put(
+    "/categories/:id", // Слушаем запросы по эндпоинту
+    updateCategory, // Обновляем запись в MongoDB
+    sendCategoryUpdated // Возвращаем ответ на клиент
+  ); 
 
 // Экспортируем роут для использования в приложении — app.js
 module.exports = categoriesRouter;

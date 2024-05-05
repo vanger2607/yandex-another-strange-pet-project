44f9e93f0e31ff729e378339e1393eac401011a5
vanger2607 <vanger2607@gmail.com>
2024-05-06 01:39:43 +0300

    feat: update routes: add validators

--- routes/games.js
+++ routes/games.js
@@ -1,21 +1,49 @@
-const gamesRouter = require('express').Router();
+const gamesRouter = require("express").Router();
 
-const {findAllGames, createGame, findGameById, updateGame, deleteGame} = require('../midllewares');
-const {sendAllGames, sendGameCreated, sendGameById, sendGameUpdated, sendGameDeleted} = require('../controllers');
+const {
+    findAllGames,
+    createGame,
+    findGameById,
+    updateGame,
+    deleteGame,
+} = require("../midllewares");
+const {
+    sendAllGames,
+    sendGameCreated,
+    sendGameById,
+    sendGameUpdated,
+    sendGameDeleted,
+} = require("../controllers");
+const {
+    checkIsGameExists,
+    checkIfCategoriesAvaliable,
+    checkEmptyFields,
+    checkIfUsersAreSafe,
+} = require("../midllewares/validators");
+gamesRouter.get("/games", findAllGames, sendAllGames);
+gamesRouter.get("/games/:id", findGameById, sendGameById);
+gamesRouter.post(
+    "/games",
+    findAllGames,
+    checkIsGameExists,
+    checkIfCategoriesAvaliable,
+    checkEmptyFields,
+    createGame,
+    sendGameCreated
+);
 
-gamesRouter.get('/games', findAllGames, sendAllGames);
-gamesRouter.get("/games/:id", findGameById, sendGameById); 
-gamesRouter.post('/games', findAllGames, createGame, sendGameCreated);
 gamesRouter.put(
-    "/games/:id", // Слушаем запросы по эндпоинту  
-      findGameById, // Шаг 1. Находим игру по id из запроса
-      // Шаг 2. Проверки, если нужны
-      updateGame, // Шаг 3. Обновляем запись с игрой
-      sendGameUpdated // Шаг 4. Возвращаем на клиент ответ с результатом обновления
-  ); 
-  gamesRouter.delete(
+    "/games/:id",
+    findGameById,
+    checkIfUsersAreSafe,
+    checkIfCategoriesAvaliable,
+    checkEmptyFields,
+    updateGame,
+    sendGameUpdated
+);
+gamesRouter.delete(
     "/games/:id", // Слушаем запросы по эндпоинту
     deleteGame,
     sendGameDeleted // Тут будут функция удаления элементов из MongoDB и ответ клиенту
-  ); 
-module.exports = gamesRouter; 
+);
+module.exports = gamesRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,49 @@
-// Создаём роут для запросов категорий 
-const usersRouter = require('express').Router();
+// Создаём роут для запросов категорий
+const usersRouter = require("express").Router();
 
 // Импортируем вспомогательные функции
-const { findAllUsers, createUser, findUserById, updateUser, deleteUser } = require('../midllewares');
-
-const { sendAllUsers, sendUserCreated, sendUserById, sendUserUpdated, sendUserDeleted } = require('../controllers');
-
-// Обрабатываем GET-запрос с роутом '/categories'
-usersRouter.get('/users',
+const {
     findAllUsers,
-    sendAllUsers);
-usersRouter.get("/users/:id",
+    createUser,
     findUserById,
-    sendUserById);
+    updateUser,
+    deleteUser,
+} = require("../midllewares");
+
+const {
+    sendAllUsers,
+    sendUserCreated,
+    sendUserById,
+    sendUserUpdated,
+    sendUserDeleted,
+} = require("../controllers");
+
+const {
+    checkIsUserExists,
+    checkEmptyNameAndEmailAndPassword,
+    checkEmptyNameAndEmail,
+} = require("../midllewares/validators");
+// Обрабатываем GET-запрос с роутом '/categories'
+usersRouter.get("/users", findAllUsers, sendAllUsers);
+usersRouter.get("/users/:id", findUserById, sendUserById);
+
 usersRouter.post(
     "/users",
     findAllUsers,
+    checkIsUserExists,
+    checkEmptyNameAndEmailAndPassword,
     createUser,
     sendUserCreated
 );
 
 usersRouter.put(
-    "/users/:id", // Слушаем запросы по эндпоинту
-    updateUser, // Обновляем запись в MongoDB
-    sendUserUpdated // Возвращаем ответ на клиент
+    "/users/:id",
+    checkEmptyNameAndEmail,
+    updateUser,
+    sendUserUpdated
 );
 
-usersRouter.delete("/users/:id",
-    deleteUser,
-    sendUserDeleted);
+usersRouter.delete("/users/:id", deleteUser, sendUserDeleted);
 
 // Экспортируем роут для использования в приложении — app.js
 module.exports = usersRouter;

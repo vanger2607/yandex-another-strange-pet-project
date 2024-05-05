406d234499c45731cc6edf85bb4091391bd4c1ab
vanger2607 <vanger2607@gmail.com>
2024-05-05 23:49:42 +0300

    feat: add put-route games/id

--- routes/games.js
+++ routes/games.js
@@ -6,4 +6,11 @@ const {sendAllGames, sendGameCreated, sendGameById} = require('../controllers');
 gamesRouter.get('/games', findAllGames, sendAllGames);
 gamesRouter.get("/games/:id", findGameById, sendGameById); 
 gamesRouter.post('/games', findAllGames, createGame, sendGameCreated);
+gamesRouter.put(
+    "/games/:id", // Слушаем запросы по эндпоинту  
+      findGameById, // Шаг 1. Находим игру по id из запроса
+      // Шаг 2. Проверки, если нужны
+      updateGame, // Шаг 3. Обновляем запись с игрой
+      sendGameUpdated // Шаг 4. Возвращаем на клиент ответ с результатом обновления
+  ); 
 module.exports = gamesRouter; 

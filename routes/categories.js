0ee80590d37edfd559576572f1adb75dd3e1e088
vanger2607 <vanger2607@gmail.com>
2024-05-06 00:27:24 +0300

    feat: add delete-route categories/id

--- routes/categories.js
+++ routes/categories.js
@@ -2,13 +2,17 @@
 const categoriesRouter = require('express').Router();
 // Импортируем вспомогательные функции
 
-const { findAllCategories, createCategory, findCategoryById, updateCategory } = require('../midllewares');
+const { findAllCategories, createCategory, findCategoryById, updateCategory, deleteCategory } = require('../midllewares');
 
-const { sendAllCategories, sendCategoryCreate, sendCategoryById, sendCategoryUpdated } = require('../controllers');
+const { sendAllCategories, sendCategoryCreate, sendCategoryById, sendCategoryUpdated, sendCategoryDeleted } = require('../controllers');
 
 // Обрабатываем GET-запрос с роутом '/categories'
-categoriesRouter.get('/categories', findAllCategories, sendAllCategories);
-categoriesRouter.get("/categories/:id", findCategoryById, sendCategoryById);
+categoriesRouter.get('/categories',
+    findAllCategories,
+    sendAllCategories);
+categoriesRouter.get("/categories/:id",
+    findCategoryById,
+    sendCategoryById);
 categoriesRouter.post(
     "/categories",
     findAllCategories,
@@ -19,7 +23,9 @@ categoriesRouter.put(
     "/categories/:id", // Слушаем запросы по эндпоинту
     updateCategory, // Обновляем запись в MongoDB
     sendCategoryUpdated // Возвращаем ответ на клиент
-  ); 
-
+);
+categoriesRouter.delete("/categories/:id",
+    deleteCategory,
+    sendCategoryDeleted);
 // Экспортируем роут для использования в приложении — app.js
 module.exports = categoriesRouter;

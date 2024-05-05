a2b248f3a87a97d1e1088cf9fc66e91418fe61d7
vanger2607 <vanger2607@gmail.com>
2024-05-05 21:12:49 +0300

    feat: add post rout

--- routes/games.js
+++ routes/games.js
@@ -1,8 +1,8 @@
 const gamesRouter = require('express').Router();
 
-const findAllGames = require('../midllewares').findAllGames;
-const sendAllGames = require('../controllers').sendAllGames;
+const {findAllGames, createGame} = require('../midllewares');
+const {sendAllGames, sendGameCreated} = require('../controllers');
 
 gamesRouter.get('/games', findAllGames, sendAllGames);
-
+gamesRouter.post('/games', findAllGames, createGame, sendGameCreated);
 module.exports = gamesRouter; 

a573d2f3dc73c5c5613029a1161e384b18778eba
vanger2607 <vanger2607@gmail.com>
2024-05-06 00:28:42 +0300

    feat: add delete-route users/id

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,17 @@
 const usersRouter = require('express').Router();
 
 // Импортируем вспомогательные функции
-const { findAllUsers, createUser, findUserById, updateUser } = require('../midllewares');
+const { findAllUsers, createUser, findUserById, updateUse, deleteUser } = require('../midllewares');
 
-const { sendAllUsers, sendUserCreated, sendUserById, sendUserUpdated } = require('../controllers');
+const { sendAllUsers, sendUserCreated, sendUserById, sendUserUpdated, sendUserDeleted } = require('../controllers');
 
 // Обрабатываем GET-запрос с роутом '/categories'
-usersRouter.get('/users', findAllUsers, sendAllUsers);
-usersRouter.get("/users/:id", findUserById, sendUserById);
+usersRouter.get('/users',
+    findAllUsers,
+    sendAllUsers);
+usersRouter.get("/users/:id",
+    findUserById,
+    sendUserById);
 usersRouter.post(
     "/users",
     findAllUsers,
@@ -20,7 +24,11 @@ usersRouter.put(
     "/users/:id", // Слушаем запросы по эндпоинту
     updateUser, // Обновляем запись в MongoDB
     sendUserUpdated // Возвращаем ответ на клиент
-  ); 
+);
+
+usersRouter.delete("/users/:id",
+    deleteUser,
+    sendUserDeleted);
 
 // Экспортируем роут для использования в приложении — app.js
 module.exports = usersRouter;

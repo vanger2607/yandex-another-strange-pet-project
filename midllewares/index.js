28ec373176f05292659efdbb8a645a92f4e08dd8
vanger2607 <vanger2607@gmail.com>
2024-05-05 20:38:27 +0300

    feat: add createUser to index.js

--- midllewares/index.js
+++ midllewares/index.js
@@ -1,4 +1,4 @@
-const findAllUsers = require("./users");
-const {findAllCategories, createCategory} = require("./categories");
-const {findAllGames, createGame} = require("./games")
-module.exports = { findAllCategories, createCategory, findAllUsers, findAllGames, createGame };
+const { findAllUsers, createUser } = require("./users");
+const { findAllCategories, createCategory } = require("./categories");
+const { findAllGames, createGame } = require("./games")
+module.exports = { findAllCategories, createCategory, findAllUsers, createUser, findAllGames, createGame };

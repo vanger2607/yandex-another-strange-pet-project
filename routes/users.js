233541b1ca8446e28c4dbc37da85fb849c9c7931
vanger2607 <vanger2607@gmail.com>
2024-05-05 23:36:32 +0300

    feat: add get-route users/id

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,19 @@
- // Создаём роут для запросов категорий 
- const usersRouter = require('express').Router();
-  
- // Импортируем вспомогательные функции
- const {findAllUsers, createUser} = require('../midllewares');
+// Создаём роут для запросов категорий 
+const usersRouter = require('express').Router();
 
- const {sendAllUsers, sendUserCreated} = require('../controllers');
- 
- // Обрабатываем GET-запрос с роутом '/categories'
- usersRouter.get('/users', findAllUsers, sendAllUsers);
- usersRouter.post(
+// Импортируем вспомогательные функции
+const { findAllUsers, createUser, findUserById } = require('../midllewares');
+
+const { sendAllUsers, sendUserCreated, sendUserById } = require('../controllers');
+
+// Обрабатываем GET-запрос с роутом '/categories'
+usersRouter.get('/users', findAllUsers, sendAllUsers);
+usersRouter.get("/users/:id", findUserById, sendUserById);
+usersRouter.post(
     "/users",
     findAllUsers,
     createUser,
     sendUserCreated
-  ); 
- // Экспортируем роут для использования в приложении — app.js
- module.exports = usersRouter;
+);
+// Экспортируем роут для использования в приложении — app.js
+module.exports = usersRouter;

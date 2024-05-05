c1f6fa2db7bdb6fdf36f700f664f46dafb0d20ed
vanger2607 <vanger2607@gmail.com>
2024-05-06 01:08:33 +0300

    feat: add checkIsCategoryExists

--- midllewares/validators/index.js
+++ midllewares/validators/index.js
@@ -1,6 +1,8 @@
 const {checkEmptyFields, checkIfCategoriesAvaliable, checkIfUsersAreSafe} = require("./games")
+const checkIsCategoryExists = require("./categories")
 module.exports = {
     checkEmptyFields,
     checkIfCategoriesAvaliable,
-    checkIfUsersAreSafe
+    checkIfUsersAreSafe,
+    checkIsCategoryExists,
 };

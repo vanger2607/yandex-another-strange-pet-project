ec6b1961ccd4734b2d9e4b822fedf0cdbcdf4bb0
vanger2607 <vanger2607@gmail.com>
2024-05-06 01:25:52 +0300

    feat: add checkIsGameExists validator

--- midllewares/validators/categories.js
+++ midllewares/validators/categories.js
@@ -2,14 +2,23 @@ const checkIsCategoryExists = async (req, res, next) => {
     // Среди существующих в базе категорий пытаемся найти категорию с тем же именем,
     // с которым хотим создать новую категорию
     const isInArray = req.categoriesArray.find((category) => {
-      return req.body.name === category.name;
+        return req.body.name === category.name;
     });
     // Если нашли совпадение, то отвечаем кодом 400 и сообщением
     if (isInArray) {
-      res.setHeader("Content-Type", "application/json");
-          res.status(400).send(JSON.stringify({ message: "Категория с таким названием уже существует" }));
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Категория с таким названием уже существует" }));
     } else {
-    // Если категория, которую хотим создать, действительно новая, то передаём управление дальше
-      next();
+        // Если категория, которую хотим создать, действительно новая, то передаём управление дальше
+        next();
     }
-  }; 
+};
+const checkIfCategoriesAvaliable = async (req, res, next) => {
+    if (!req.body.categories || req.body.categories.length === 0) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Выберите хотя бы одну категорию" }));
+    } else {
+        next();
+    }
+};
+module.exports = { checkIsCategoryExists, checkIfCategoriesAvaliable }
--- midllewares/validators/index.js
+++ midllewares/validators/index.js
@@ -1,5 +1,5 @@
 const {checkEmptyFields, checkIfCategoriesAvaliable, checkIfUsersAreSafe, checkIsGameExists} = require("./games")
-const checkIsCategoryExists = require("./categories");
+const {checkIsCategoryExists, checkIfCategoriesAvaliable} = require("./categories");
 const {checkEmptyNameAndEmailAndPassword, checkEmptyNameAndEmail, checkIsUserExists} = require("./users");
 module.exports = {
     checkEmptyFields,
@@ -7,6 +7,7 @@ module.exports = {
     checkIfUsersAreSafe,
     checkIfCategoriesAvaliable,
     checkIsCategoryExists,
+    checkIfCategoriesAvaliable,
     checkEmptyNameAndEmailAndPassword,
     checkEmptyNameAndEmail,
     checkIsUserExists,

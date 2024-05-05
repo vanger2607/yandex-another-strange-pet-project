e970bf623d658735ed4eff115755089de36e17fd
vanger2607 <vanger2607@gmail.com>
2024-05-05 20:33:51 +0300

    feat: create createCategory midlleware

--- midllewares/categories.js
+++ midllewares/categories.js
@@ -1,11 +1,22 @@
 // Импортируем модель
 const categories = require("../models/category");
+const logger = require("@/logger")
 
 const findAllCategories = async (req, res, next) => {
-  // По GET-запросу на эндпоинт /categories найдём все документы категорий
-  req.categoriesArray = await categories.find({});
-  next();
+    // По GET-запросу на эндпоинт /categories найдём все документы категорий
+    req.categoriesArray = await categories.find({});
+    next();
 };
-
+const createCategory = async (req, res, next) => {
+    logger.info("POST /categories");
+    try {
+        logger.info(req.body);
+        req.category = await categories.create(req.body);
+        next();
+    } catch (error) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send(JSON.stringify({ message: "Ошибка создания категории" }));
+    }
+};
 // Экспортируем функцию поиска всех категорий
-module.exports = findAllCategories;
+module.exports = { findAllCategories, createCategory };

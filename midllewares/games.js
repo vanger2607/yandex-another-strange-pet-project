d7159f1a072caf511d90e4ac7cced5f7858041b9
vanger2607 <vanger2607@gmail.com>
2024-05-05 20:29:40 +0300

    feat: create createGame midlleware

--- midllewares/games.js
+++ midllewares/games.js
@@ -1,12 +1,23 @@
 const games = require("../models/game");
-
+const logger = require("@/logger")
 const findAllGames = async (req, res, next) => {
   // По GET-запросу на эндпоинт /games найдём все документы категорий
   req.gamesArray = await games.find({}).populate("categories").populate("users");;
-  // Выведем в терминал результат поиска 
-  console.log(req.gamesArray);
+  logger.info(req.gamesArray);
   next();
 };
+const createGame = async (req, res, next) => {
+    logger.info("POST /games");
+    try {
+      logger.info(req.body);
+      req.game = await games.create(req.body);
+      next();
+    } catch (error) {
+        res.setHeader("Content-Type", "application/json");
+          res.status(400).send(JSON.stringify({ message: "Ошибка создания игры" }));
+    }
+  }; 
+
 
 // Экспортируем функцию поиска всех игр
-module.exports = findAllGames; 
+module.exports = {findAllGames, createGame}; 

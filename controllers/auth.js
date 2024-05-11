0d37efe034a5a90b3aa1a47f5315ce97bd4929e7
vanger2607 <vanger2607@gmail.com>
2024-05-11 14:24:19 +0300

    refactor: add sign token to login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,28 @@
 const users = require("../models/user.js");
-
+const jwt = require("jsonwebtoken");
+const crypt = require("crypto");
 const login = (req, res) => {
     const { email, password } = req.body;
 
     users
         .findUserByCredentials(email, password)
         .then((user) => {
-            res
-                .status(200)
-                .send({ _id: user._id, username: user.username, email: user.email });
+            const secretKey = crypt.randomBytes(256).toString("base64");
+            const token = jwt.sign({ _id: user._id }, secretKey, {
+                expiresIn: 3600,
+            });
+            return { user, token };
+        })
+        .then(({ user, token }) => {
+            res.status(200).send({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                jwt: token,
+            });
         })
         .catch((error) => {
             res.status(401).send({ message: error.message });
         });
 };
-
-// Не забываем экспортирвать функцию
 module.exports = login;

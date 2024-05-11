d05a40b81f4ff269f68f8d8d7dce5812c37903ec
vanger2607 <vanger2607@gmail.com>
2024-05-11 15:38:39 +0300

    refactor: refac sendIndex: if no cookies jwt also send index.html

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,6 +42,7 @@ const sendIndex = (req, res) => {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         }
     }
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 };
 
 module.exports = {

71ac63303f5b71b7edbf90f1e33330940e12fe86
vanger2607 <vanger2607@gmail.com>
2024-05-11 14:01:30 +0300

    refactor: take out buisness logic from controller to static method in users model

--- models/user.js
+++ models/user.js
@@ -1,20 +1,39 @@
-const mongoose = require('mongoose');
- 
+const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
 });
 
+userSchema.statics.findUserByCredentials = function (email, password) {
+    // Попытаемся найти пользователя по почте
+    return this.findOne({ email }) // this — это модель users
+        .then((user) => {
+            if (!user) {
+                // Не нашёлся — отклоняем промис
+                return Promise.reject(new Error("Неправильные почта или пароль"));
+            }
+
+            // Нашёлся — сравниваем хеши
+            return bcrypt.compare(password, user.password).then((matched) => {
+                if (!matched) {
+                    return Promise.reject(new Error("Неправильные почта или пароль"));
+                }
+
+                return user; // Теперь user доступен
+            });
+        });
+};
+
 module.exports = mongoose.model("user", userSchema);
- 
